4b43638f83bca6e1c54eb35e8b40d602856982c1
LMiranda <LMiranda32PM@gmail.com>
2021-10-29 14:47:40 -0700

    script.js file

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".container");
 
+// This section opens and closes the navbar area
 document.querySelector(".open-navbar-icon").addEventListener("click", () => {
   container.classList.add("change");
 });
